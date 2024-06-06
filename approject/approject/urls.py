0c9a5aa624b8d5739ada9cf724ebe11c1807a6ae
alessia <alessia.profeta@gmail.com>
2024-06-06 10:35:48 +0200

    insert logo and favicon. Improve contact_me area

--- approject/approject/urls.py
+++ approject/approject/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("portfolio.urls")),
     path("blog/", include("blog.urls")),
 ]
+
+# Aggiungi la configurazione per servire i file statici in ambiente di sviluppo
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
